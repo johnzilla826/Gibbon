79216b05351bc4cdc5559439a139a606a6885138
johnzilla826 <229241950+johnzilla826@users.noreply.github.com>
2025-10-05 07:02:41 -0500

    chore: create entry scaffold

--- openview/accounting/urls.py
+++ openview/accounting/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "accounting"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("trial-balance/", views.trial_balance, name="trial-balance")
+    path("reports/trial-balance/", views.trial_balance, name="trial-balance"),
+    path("create-entry/", views.create_entry, name="create-entry")
 ]
--- openview/accounting/views.py
+++ openview/accounting/views.py
@@ -4,4 +4,7 @@ def index(request):
     return render(request, "accounting/index.html")
 
 def trial_balance(request):
-    return render(request, "accounting/trial-balance.html")
+    return render(request, "accounting/reports/trial-balance.html")
+
+def create_entry(request):
+    return render(request, "accounting/create-entry.html")
