e5e18bcb341965137fe0653d33df5222c762f638
johnzilla826 <229241950+johnzilla826@users.noreply.github.com>
2025-10-05 06:30:03 -0500

    feat: initial trial balance scaffold

--- gibbon/accounting/urls.py
+++ gibbon/accounting/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "accounting"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("trial-balance/", views.trial_balance, name="trial-balance")
 ]
--- gibbon/accounting/views.py
+++ gibbon/accounting/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "accounting/index.html")
+
+def trial_balance(request):
+    return render(request, "accounting/trial-balance.html")
